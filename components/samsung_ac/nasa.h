e0174d6f75969250b18b3bfa8bfd135e4a1932d4
Foxhill67 <35631055+Foxhill67@users.noreply.github.com>
2023-12-15 23:59:25 +0100

    3

--- components/samsung_ac/nasa.h
+++ components/samsung_ac/nasa.h
@@ -168,7 +168,7 @@ namespace esphome
             std::vector<uint8_t> get_target_temp_message(const std::string &address, float value) override;
             std::vector<uint8_t> get_mode_message(const std::string &address, Mode value) override;
             std::vector<uint8_t> get_fanmode_message(const std::string &address, FanMode value) override;
-            std::vector<uint8_t> get_water_power_heater_message(const std::string &address, bool value);
+            std::vector<uint8_t> get_water_power_heater_message(const std::string &address, bool value) override;
         };
 
     } // namespace samsung_ac
