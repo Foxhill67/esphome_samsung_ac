9fc1fe9d26c66e7cbb41f46ee82b95fcd94ee678
Foxhill67 <35631055+Foxhill67@users.noreply.github.com>
2024-01-04 10:24:26 +0100

    remove message

--- components/samsung_ac/nasa.cpp
+++ components/samsung_ac/nasa.cpp
@@ -756,7 +756,7 @@ namespace esphome
 					}	
 					default:	
 					{	
-                         ESP_LOGW(TAG, "Unknown message s:%s d:%s %d %d", packet_.sa.to_string().c_str(), packet_.da.to_string().c_str(), message.messageNumber, message.value);																															
+//                         ESP_LOGW(TAG, "Unknown message s:%s d:%s %d %d", packet_.sa.to_string().c_str(), packet_.da.to_string().c_str(), message.messageNumber, message.value);																															
 						break;
 					}																																																	
                 } 
